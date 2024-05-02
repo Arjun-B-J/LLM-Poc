70510d86fb4a223000cf3e37e9db7bd86f5e6224
Arjun BJ <47909735+Arjun-B-J@users.noreply.github.com>
2024-05-02 12:45:12 +0530

    Update

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
+import os
+os.environ['HF_HOME'] = 'D:\\llm\\cache'
+os.environ['TRANSFORMERS_CACHE'] = 'D:\\llm\\cache'
 from dotenv import find_dotenv, load_dotenv
 from transformers import pipeline
 
 load_dotenv(find_dotenv())
 
-# img2txt
-
-def img2txt(url):
-    image_to_text = pipeline("image-to-text",model="Salesforce/blip-image-captioning-base")
-    text = image_to_text(url)
-    print(text)
-
 #sql 
 def sql():
-    pipe = pipeline("text-generation", model="defog/sqlcoder2")
+    generator = pipeline("text-generation", model="codellama/CodeLlama-7b-hf")
 
     prompt = "This is my Database schema, CREATE TABLE Student (student_id INT PRIMARY KEY AUTO_INCREMENT,first_name VARCHAR(255) NOT NULL,last_name VARCHAR(255) NOT NULL,date_of_birth DATE,email VARCHAR(255) UNIQUE,phone_number VARCHAR(20),grade_level INT,major_id INT,FOREIGN KEY (major_id) REFERENCES Major(major_id));Retrieve the total number of student names which starts with letter A."
 
-    # Generate SQL query
-    sql_query = pipe(prompt)[0]["generated_text"]
-    print(f"Generated SQL query: {sql_query}")
+    # Generate the SQL query
+    result = generator(prompt, max_length=1000, do_sample=True, temperature=0.7)
 
-    print(f"Generated SQL query: {sql_query}")
+    # Extract the generated SQL query
+    generated_query = result[0]['generated_text']
+    
+    print(result)
 
-#img2txt("img.jpg")
 sql()
